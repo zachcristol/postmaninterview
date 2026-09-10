78fc16e0ea0b0c54532cb4405e014d7242cebea4
Zach <zach@Zachs-MacBook-Pro-2.local>
2026-09-10 17:04:04 -0500

    Split app into controller/service/model/repository packages
    Sets up the conventional Spring layering ahead of the interview so
    business logic (the seed-race fix) has a home in a service layer
    instead of living in the controller.

--- pre-assessment/src/main/java/com/postman/planetexpress/repository/ShipmentRepository.java
+++ pre-assessment/src/main/java/com/postman/planetexpress/repository/ShipmentRepository.java
@@ -1,5 +1,6 @@
-package com.postman.planetexpress;
+package com.postman.planetexpress.repository;
 
+import com.postman.planetexpress.model.Shipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
